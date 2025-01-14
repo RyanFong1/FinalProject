5228bc4b0607a4149b05742a73f36b958cc63edf
Ryan Fong <ryan.fong2@student.tdsb.on.ca>
2025-01-14 12:48:58 -0500

    Initial Commit

---
+++ AppSelectorTests/AppSelectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppSelectorTests.swift
+//  AppSelectorTests
+//
+//  Created by Ryan Fong on 2025-01-14.
+//
+
+import Testing
+@testable import AppSelector
+
+struct AppSelectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
